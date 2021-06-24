85bd90c80a668a8f276e8936d5da106cc34180a4
Daniel Miller <millerpils@gmail.com>
2021-06-24 15:22:28 +0100

    Cart and other stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Plants',
-    description: 'Green',
+    name: 'Boston Fern',
+    description:
+      'Jagged green fronds - perfect for a bathroom or light windowsill.',
+    features: ['Moisture loving', 'Easy care', 'Dislikes direct sun'],
+    price: 3.99,
+    stockLevel: 0,
+    imageSrc: './images/boston_fern_sm.jpg',
+    link: '/products/boston-fern',
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
   },
 });
