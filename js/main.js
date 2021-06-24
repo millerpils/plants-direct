92d58d76da1318f1869c46f6250ad04616294873
Daniel Miller <millerpils@gmail.com>
2021-06-24 17:46:39 +0100

    Buttons and bits

--- js/main.js
+++ js/main.js
@@ -5,15 +5,31 @@ var app = new Vue({
     description:
       'Jagged green fronds - perfect for a bathroom or light windowsill.',
     features: ['Moisture loving', 'Easy care', 'Dislikes direct sun'],
-    price: 3.99,
-    stockLevel: 0,
+    price: null,
+    stockNotice: null,
+    sizes: [
+      { name: 'Small 20cm', price: 3.99, stockLevel: 0 },
+      { name: 'Medium 30cm', price: 6.99, stockLevel: 10 },
+    ],
     imageSrc: './images/boston_fern_sm.jpg',
     link: '/products/boston-fern',
     cart: 0,
   },
   methods: {
-    addToCart: function () {
+    addToCart: function (e) {
       this.cart += 1;
+    },
+    checkStock: function (size) {
+      if (size.stockLevel > 0) {
+        return this.updatePrice(size.price);
+      }
+
+      this.price = null;
+      this.stockNotice = 'Sorry, we are out of stock';
+    },
+    updatePrice: function (itemPrice) {
+      this.stockNotice = null;
+      this.price = itemPrice;
     },
   },
 });
