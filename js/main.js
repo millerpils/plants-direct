9a8ea3a0fa5660a28e20a258f91078782a988e70
Daniel Miller <millerpils@gmail.com>
2021-06-25 15:53:04 +0100

    Disabled button

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@ var app = new Vue({
   data: {
     plants: [
       {
+        productId: 1,
         name: 'Boston Fern',
         description:
           'Jagged green fronds - perfect for a bathroom or light windowsill.',
         features: ['Moisture loving', 'Easy care', 'Dislikes direct sun'],
         variants: [
-          { id: 1, name: 'Small 20cm', price: 3.99, stockLevel: 0 },
+          { id: 1, name: 'Small 20cm', price: 3.99, stockLevel: 2 },
           { id: 2, name: 'Medium 30cm', price: 6.99, stockLevel: 10 },
         ],
-        selectedVariant: 1,
+        selectedVariant: null,
         images: [
           {
             id: 1,
@@ -28,16 +29,19 @@ var app = new Vue({
         ],
         price: null,
         link: '/products/boston-fern',
+        addedToCart: false,
       },
     ],
-    cart: 0,
+    cart: [],
   },
   methods: {
-    addToCart() {
-      this.cart += 1;
+    addToCart(plant) {
+      this.cart.push(plant.id);
+      plant.addedToCart = true;
     },
-    updatePrice(plant, price) {
+    updatePrice(plant, price, index) {
       plant.price = price;
+      plant.selectedVariant = index + 1;
     },
   },
 });
