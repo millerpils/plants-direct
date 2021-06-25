468d573caf97b7378b020a6d9538d463961f5e0e
Daniel Miller <millerpils@gmail.com>
2021-06-25 17:17:46 +0100

    Revert - don't make it too complex!

--- js/main.js
+++ js/main.js
@@ -27,6 +27,56 @@ var app = new Vue({
         link: '/products/boston-fern',
         addedToCart: false,
       },
+      {
+        productId: 2,
+        name: 'Maidenhair Fern',
+        description:
+          'Jagged green fronds - perfect for a bathroom or light windowsill.',
+        features: ['Moisture loving', 'Easy care', 'Dislikes direct sun'],
+        stockLevel: 5,
+        images: [
+          {
+            id: 1,
+            size: 'small',
+            title: 'Small Boston Fern',
+            imageSrc: './images/boston_fern_sm.jpg',
+          },
+          {
+            id: 2,
+            size: 'large',
+            title: 'Large Boston Fern',
+            imageSrc: './images/boston_fern_lg.jpg',
+          },
+        ],
+        price: null,
+        link: '/products/boston-fern',
+        addedToCart: false,
+      },
+      {
+        productId: 3,
+        name: 'Tree Fern',
+        description:
+          'Jagged green fronds - perfect for a bathroom or light windowsill.',
+        features: ['Moisture loving', 'Easy care', 'Dislikes direct sun'],
+        stockLevel: 5,
+        images: [
+          {
+            id: 1,
+            size: 'small',
+            title: 'Small Boston Fern',
+            imageSrc: './images/boston_fern_sm.jpg',
+          },
+          {
+            id: 2,
+            size: 'large',
+            title: 'Large Boston Fern',
+            imageSrc: './images/boston_fern_lg.jpg',
+          },
+        ],
+        price: null,
+        link: '/products/boston-fern',
+        addedToCart: false,
+      },
     ],
     cart: [],
   },
